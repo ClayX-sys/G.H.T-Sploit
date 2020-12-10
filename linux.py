335ca929875e60c63e198d55ba3e2dba56edbf8c
ClayX-sys <75772922+ClayX-sys@users.noreply.github.com>
2020-12-10 09:39:03 +0400

    Create linux.py

---
+++ linux.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import time
+os.system("clear")
+banner="""
+\033[32;40;1m
+########################################
+      👿Gok Turan Hack Team👿 
+       Coder > @clayx.sc           
+       CybeSecurity           
+########################################
+"""
+print (banner)
+print ("\033[32;40;1m""[1]""\033[33;40;1m""Dos Ddos Saldırısı")
+print ("\033[32;40;1m""[2]""\033[33;40;1m""Toplu Hack Aracı")
+print ("\033[32;40;1m""[3]""\033[33;40;1m""Konum Tespit")
+print ("\033[32;40;1m""[4]""\033[33;40;1m""Metasploiti Ac")
+print ("\033[32;40;1m""[5]""\033[33;40;1m""Arama Spam")
+print ("\033[32;40;1m""[6]""\033[33;40;1m""Port Tarama")
+print ("\033[32;40;1m""[99]""\033[33;40;1m""Cıkış Yap")
+print ("\033[32;40;1m""[00]""\033[33;40;1m""Sistemi Guncelle")
+
+islem=int(input("\033[33;40;1m""Işlem Numarası: "))
+if islem==1:os.system("python2 .py")
+elif islem==2:os.system("bash ..sh")
+elif islem==3:os.system("python2 ...py")
+elif islem==4:os.system("pkg install metasploit && msfconsole")
+elif islem==5:os.system("python2 .....py")
+elif islem==6:os.system("python2 ......py")
+elif islem==99:os.system("exit")
+elif islem==00:os.system("pkg install && pkg upgrade && pkg install git && pkg install python && pkg install python2 && pkg install python3 && pkg install bs4 && pkg install perl && pkg install metasploit && pkg install ruby")
